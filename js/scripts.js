25cecdde5c38709258d0a7b85cac5ed4c58e8b6d
Alexander Slesarenko <kettyleegifford@yandex.ru>
2017-06-25 20:18:02 +0300

    ...

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ var nav = $('#nav');
 
 $(document).ready(function(){
     set_float_nav();
-    // set_form_submit_listener();
+    set_form_submit_listener();
     set_phone_mask();
     set_toastr_options();
 });
@@ -19,38 +19,38 @@ function set_toastr_options() {
 function set_phone_mask() {
     $('[name="phone"]').mask("+7 (999) 999-99-99");
 }
-// function set_form_submit_listener() {
-//     $('form').submit(function(){
-//         var that = this;
-//         $(this).find('button[type="submit"]').prop('disabled', true);
-//         var name = $(this).find('input[name="name"]').val();
-//         var phone = $(this).find('input[name="phone"]').val();
-//         if (!name.replace(/ /g,'')) {
-//             toastr.info('Пожалуйста, укажите Ваше имя.');
-//             $(this).find('button[type="submit"]').prop('disabled', false);
-//             return false;
-//         }
-//         if (!phone) {
-//             toastr.info('Пожалуйста, введите номер телефона.');
-//             $(this).find('button[type="submit"]').prop('disabled', false);
-//             return false;
-//         }
-//
-// 		$.post(
-// 			'https://script.google.com/macros/s/AKfycbyD9RuAa3-QiToUOgV3_JKOzCkdIRNfFMP5hHHkHCokqd2yhpw/exec',
-// 			{
-// 				name : name,
-// 				phone : phone
-// 			}, function(){
-//                 $(that).find('input[name="name"]').val('');
-//                 $(that).find('input[name="phone"]').val('');
-//                 $(that).find('button[type="submit"]').prop('disabled', false);
-// 				toastr.success('Заявка успешно отправлена.');
-// 			}
-// 		);
-// 		return false;
-// 	});
-// }
+function set_form_submit_listener() {
+    $('form').submit(function(){
+        var that = this;
+        $(this).find('button[type="submit"]').prop('disabled', true);
+        var name = $(this).find('input[name="name"]').val();
+        var phone = $(this).find('input[name="phone"]').val();
+        if (!name.replace(/ /g,'')) {
+            toastr.info('Пожалуйста, укажите Ваше имя.');
+            $(this).find('button[type="submit"]').prop('disabled', false);
+            return false;
+        }
+        if (!phone) {
+            toastr.info('Пожалуйста, введите номер телефона.');
+            $(this).find('button[type="submit"]').prop('disabled', false);
+            return false;
+        }
+
+		$.post(
+            'https://script.google.com/macros/s/AKfycbw9iZeumDlu_sCtBHci8hp4Zf6S2EbK87ncis8J6t7fTWH4fYQ/exec',
+			{
+				name : name,
+				phone : phone
+			}, function(){
+                $(that).find('input[name="name"]').val('');
+                $(that).find('input[name="phone"]').val('');
+                $(that).find('button[type="submit"]').prop('disabled', false);
+				toastr.success('Заявка успешно отправлена.');
+			}
+		);
+		return false;
+	});
+}
 function set_float_nav() {
     $(window).scroll(function() {
         var scroll_top = $(window).scrollTop();
